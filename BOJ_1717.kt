18708ea6c4bcb5847a721394d21265e90d3a894c
esg111 <tjs5143a@gmail.com>
2023-01-29 23:42:52 +0900

    CODING_230129

---
+++ BOJ_1717.kt
@@ -0,0 +1,23 @@
+fun main() {
+    val (n, m) = readLine()!!.split(' ').map { it.toInt() }
+    val node = IntArray(n + 1) { it }
+
+    fun find(b:Int): Int{
+        if(node[b]==b) return b
+        node[b] = find(node[b])
+        return node[b]
+    }
+
+    fun union(b:Int, c:Int){
+        node[find(c)] = find(b)
+    }
+
+    repeat(m) {
+        val (a, b, c) = readLine()!!.split(' ').map { it.toInt() }
+        when(a) {
+            0 -> union(b, c)
+            1 -> println(if (find(b) == find(c)) "YES" else "NO")
+        }
+    }
+
+}
